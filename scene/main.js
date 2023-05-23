da479d6ff7e22e7455d8076c2c07053b778a24ea
New-Connected <123391187+CharliVanNood@users.noreply.github.com>
2023-05-23 22:37:29 +0200

    updated demo scene

--- scene/main.js
+++ scene/main.js
@@ -1,29 +1,36 @@
-var background;
-var rect1;
-var player;
+const obstacles = [
+    [100, -275], [200, -275], [200, -225]
+]
 
 function start() {
     background = create_rect(
         "background", [0, 0], 0, [20000, 2000], [222, 75, 217]
         );
-    rect1 = create_rect(
+    floor = create_rect(
         "rect1", [0, -500], 0, [20000, 400], [255, 0, 255]
         );
     player = create_rect(
-        "player", [-800, 0], 0, [50, 50], [255, 255, 255]
+        "player", [0, 0], 0, [50, 50], [255, 255, 255]
         );
     player_center = create_rect(
-        "player", [-800, 0], 0, [40, 40], [255, 0, 255]
+        "player", [0, 0], 0, [40, 40], [255, 0, 255]
         );
+    
+    for (let obstacle = 0; obstacle < obstacles.length; obstacle++) {
+        create_rect(
+            "obstacle", 
+            obstacles[obstacle], 0, 
+            [50, 50], [255, 255, 255]
+            );
+    }
 
-    //transform(rect1, [0, 50], 0);
     set_gui("fps", true);
     set_fps(120);
 }
 
 function update() {
     transform(player, 
-        [get_pos(player)[0] + 1, get_pos(player)[1]],
+        [get_pos(player)[0] + 0, get_pos(player)[1]],
         0
         )
     transform(player_center, get_pos(player), 0)
