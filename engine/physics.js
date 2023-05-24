4d0f9502faebc0300e5e30e9a93ba29a06b0a662
New-Connected <88381799+New-Connected@users.noreply.github.com>
2023-05-24 09:51:47 +0200

    simple collider update

--- engine/physics.js
+++ engine/physics.js
@@ -1,4 +1,4 @@
-function gravity(object) {
+function box_collider(object) {
     let can_move = true;
     let entities = window.entities;
     for (let entity = 0; entity < entities.length; entity++) {
@@ -7,16 +7,36 @@ function gravity(object) {
                 if (entities[entity][8][modifier][0] == "collider") {
                     if (entities[entity][1][0] - entities[entity][3][0] / 2 < object[1][0] + object[3][0] / 2 && 
                         entities[entity][1][0] + entities[entity][3][0] / 2 > object[1][0] - object[3][0] / 2 &&
+                        entities[entity][1][1] - entities[entity][3][1] / 2 < object[1][1] + object[3][1] / 2 &&
                         entities[entity][1][1] + entities[entity][3][1] / 2 > object[1][1] - object[3][1] / 2) {
                         can_move = false
+                        
+                        if (entities[entity][1][0] > object[1][0] + object[3][0] / 2) {
+                            object[1][0] = entities[entity][1][0] - entities[entity][3][0] / 2 - object[3][0] / 2
+                        } else 
+                        if (entities[entity][1][0] < object[1][0] - object[3][0] / 2) {
+                            object[1][0] = entities[entity][1][0] + entities[entity][3][0] / 2 + object[3][0] / 2
+                        } else 
+                        if (entities[entity][1][1] < object[1][1] - object[3][1] / 2) {
+                            object[1][1] = entities[entity][1][1] + entities[entity][3][1] / 2 + object[3][1] / 2
+                        } else 
+                        if (entities[entity][1][1] > object[1][1] - object[3][1] / 2) {
+                            object[1][1] = entities[entity][1][1] - entities[entity][3][1] / 2 - object[3][1] / 2
+                        }
                     }
                 }
             }
         }
     }
-    if (can_move) {
+    return can_move
+}
+
+function gravity(object) {
+    if (box_collider(object)) {
         object[1][1] -= object[7][1]
         object[7][1] += 0.031
+    } else {
+        object[7][1] = 0
     }
 }
 
