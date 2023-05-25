911d5530f30de4c112fabda9c0fd975f340df97a
New-Connected <88381799+New-Connected@users.noreply.github.com>
2023-05-25 11:45:27 +0200

    ok

--- engine/physics.js
+++ engine/physics.js
@@ -2,26 +2,31 @@ function box_collider(object) {
     let can_move = true;
     let entities = window.entities;
     for (let entity = 0; entity < entities.length; entity++) {
-        for (let modifier = 0; modifier < entities[entity][8].length; modifier++) {
+        if (entities[entity][6] != object[6]) {
             for (let modifier = 0; modifier < entities[entity][8].length; modifier++) {
-                if (entities[entity][8][modifier][0] == "collider") {
-                    if (entities[entity][1][0] - entities[entity][3][0] / 2 < object[1][0] + object[3][0] / 2 && 
-                        entities[entity][1][0] + entities[entity][3][0] / 2 > object[1][0] - object[3][0] / 2 &&
-                        entities[entity][1][1] - entities[entity][3][1] / 2 < object[1][1] + object[3][1] / 2 &&
-                        entities[entity][1][1] + entities[entity][3][1] / 2 > object[1][1] - object[3][1] / 2) {
-                        can_move = false
-                        
-                        if (entities[entity][1][0] > object[1][0] + object[3][0] / 2) {
-                            object[1][0] = entities[entity][1][0] - entities[entity][3][0] / 2 - object[3][0] / 2
-                        } else 
-                        if (entities[entity][1][0] < object[1][0] - object[3][0] / 2) {
-                            object[1][0] = entities[entity][1][0] + entities[entity][3][0] / 2 + object[3][0] / 2
-                        } else 
-                        if (entities[entity][1][1] < object[1][1] - object[3][1] / 2) {
-                            object[1][1] = entities[entity][1][1] + entities[entity][3][1] / 2 + object[3][1] / 2
-                        } else 
-                        if (entities[entity][1][1] > object[1][1] - object[3][1] / 2) {
-                            object[1][1] = entities[entity][1][1] - entities[entity][3][1] / 2 - object[3][1] / 2
+                for (let modifier = 0; modifier < entities[entity][8].length; modifier++) {
+                    if (entities[entity][8][modifier][0] == "collider") {
+                        if (entities[entity][1][0] - entities[entity][3][0] / 2 < object[1][0] + object[3][0] / 2 && 
+                            entities[entity][1][0] + entities[entity][3][0] / 2 > object[1][0] - object[3][0] / 2 &&
+                            entities[entity][1][1] - entities[entity][3][1] / 2 < object[1][1] + object[3][1] / 2 &&
+                            entities[entity][1][1] + entities[entity][3][1] / 2 > object[1][1] - object[3][1] / 2) {
+
+                            can_move = false
+                            
+                            if (entities[entity][1][0] > object[1][0] + object[3][0] / 2 && 
+                                object[1][0] < entities[entity][1][0] - entities[entity][3][0] / 4) {
+                                object[1][0] = entities[entity][1][0] - entities[entity][3][0] / 2 - object[3][0] / 4
+                            } else 
+                            if (entities[entity][1][0] < object[1][0] - object[3][0] / 2 && 
+                                object[1][0] > entities[entity][1][0] + entities[entity][3][0] / 4) {
+                                object[1][0] = entities[entity][1][0] + entities[entity][3][0] / 2 + object[3][0] / 2
+                            } else 
+                            if (entities[entity][1][1] > object[1][1] + object[3][1] / 2) {
+                                object[1][1] = entities[entity][1][1] - entities[entity][3][1] / 2 - object[3][1] / 2
+                            } else
+                            if (entities[entity][1][1] < object[1][1] - object[3][1] / 2) {
+                                object[1][1] = entities[entity][1][1] + entities[entity][3][1] / 2 + object[3][1] / 2
+                            }
                         }
                     }
                 }
--- scene/main.js
+++ scene/main.js
@@ -1,16 +1,16 @@
 const obstacles = [
-    [100, -275], [200, -275], [200, -225]
+    [[100, -100], [50, 500]]
 ];
 
 function start() {
     background = create_rect(
-        "background", [0, 0], 0, [20000, 2000], [50, 50, 50]
+        "background", [0, 0], 0, [100000, 100000], [50, 50, 50]
     );
     floor = create_rect(
         "rect1", [0, -500], 0, [20000, 400], [100, 100, 100]
     );
     floor_test = create_rect(
-        "rect1", [-100, -275], 0, [50, 50], [255, 255, 255]
+        "rect1", [-200, -275], 0, [50, 50], [255, 255, 255]
     );
     player = create_rect(
         "player", [0, 0], 0, [50, 50], [255, 255, 255]
@@ -22,17 +22,18 @@ function start() {
     for (let obstacle = 0; obstacle < obstacles.length; obstacle++) {
         let _obstacle = create_rect(
             "obstacle", 
-            obstacles[obstacle], 0, 
-            [50, 50], [255, 255, 255]
+            obstacles[obstacle][0], 0, 
+            obstacles[obstacle][1], [255, 255, 255]
         );
         add_modifier(_obstacle, "collider");
     }
 
     add_modifier(player, "gravity");
+    add_modifier(player, "collider");
     add_modifier(floor_test, "collider");
     add_modifier(floor, "collider");
 
-    set_gui("fps", true);
+    set_gui("fps", false);
     set_fps(120);
 }
 
@@ -43,6 +44,6 @@ function update() {
         transform(player, [pos[0] + 1, pos[1]], 0);
     }
     transform(player_center, pos, 0);
-    transform(floor_test, [pos2[0] + 0.1, pos2[1]], 0);
+    transform(floor_test, [pos2[0] + 0.5, pos2[1]], 0);
     transform("camera", pos, 0);
 }
